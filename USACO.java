e8749dc2a5e79ccb2294a7d3d2d5bdaa764278f0
Hilary Zen <hzen00@stuy.edu>
2019-03-06 10:58:19 -0500

    Added new fileReader method

--- USACO.java
+++ USACO.java
@@ -2,8 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class USACO {
-  //Bronze Problem 12: Lake Making
-  public static int bronze(String filename) {
+  private static ArrayList<String> fileReader(String filename) {
     ArrayList<String> lines = new ArrayList<String>();
     try {
       File text = new File(filename);
@@ -14,11 +13,18 @@ public class USACO {
     } catch (FileNotFoundException e) {
       System.out.println("Invalid file: " + filename);
     }
+    return lines;
+  }
+
+  //Bronze Problem 12: Lake Making
+  public static int bronze(String filename) {
+    ArrayList<String> lines = fileReader(filename);
     System.out.println(lines);
     return 0;
   }
 
   public static int silver(String filename) {
+    ArrayList<String> lines = fileReader(filename);
     return 0;
   }
 
