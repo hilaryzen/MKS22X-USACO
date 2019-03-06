698c8108fc9868f15cb9b710a14cf08f27168cfd
Hilary Zen <hzen00@stuy.edu>
2019-03-06 10:35:42 -0500

    Started file

---
+++ USACO.java
@@ -0,0 +1,10 @@
+public class USACO {
+  //Bronze Problem 12: Lake Making
+  public static int bronze(String filename) {
+    return 0;
+  }
+
+  public static int silver(String filename) {
+    return 0;
+  }
+}
